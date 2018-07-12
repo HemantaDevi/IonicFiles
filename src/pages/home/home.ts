cce0f0f7cdd12c645a112e077bc3d4c5511e14bc
Huril <hemanta.devi.huril@accenture.com>
2018-07-12 13:45:32 +0400

    IonicFile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+import { QuotesPage } from '../quotes/quotes';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +13,28 @@ export class HomePage {
 
   }
 
+  GoToProfile(){
+    console.log("This is magic..!!")
+    this.navCtrl.push(ProfilePage)
+  }
+
+  callPhone(PhoneNumber:string){
+    console.log("Calling Yaya..");
+    window.location.href="tel: "+ encodeURIComponent(PhoneNumber);
+  }
+
+  sendMail(emailAddress:string){
+    console.log("Mailing hema..");
+    window.location.href="mailto: "+ encodeURIComponent(emailAddress);
+  }
+
+  sendSMS(sms:string){
+    console.log("sending sms..");
+    window.location.href="sms: "+ encodeURIComponent(sms);
+  }
+
+  GoToQuotesPage(){
+    console.log("This is magic..!!")
+    this.navCtrl.push(QuotesPage);
+  }
 }
